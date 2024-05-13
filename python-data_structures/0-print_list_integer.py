7eee28fb9329ee8ec78a1636cbaa258fcf47b758
paulineonier <pauline.onier@outlook.fr>
2024-05-13 09:23:47 +0000

    task 0,1,3,4

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
-
-    length = len(my_list)
-
-    if idx > length - 1:
-        return (my_list)
-
-    my_list[idx] = element
-
-    return (my_list)
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
