70de6d23d858ea61f4efc8ebb0861604d8881e73
paulineonier <pauline.onier@outlook.fr>
2024-05-14 11:52:33 +0000

    task 1 correction

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None:
-        return([x if x != search else replace for x in my_list])
-    return None
+    return [replace if x == search else x for x in my_list]
