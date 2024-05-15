78a9df2db88f35085aaede83c0817f5469aa5570
paulineonier <pauline.onier@outlook.fr>
2024-05-15 17:00:24 +0000

    task 0 correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    idx = 0
+
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-    except IndexError:
+        for i in my_list:
+            if idx < x:
+                print('{}'.format(my_list[idx]), end='')
+                idx += 1
+
+        print()
+    except TypeError:
         pass
-    print()
-    return count
+    finally:
+        return idx
