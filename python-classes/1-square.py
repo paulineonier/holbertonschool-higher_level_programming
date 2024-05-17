d263883c802121439b6e3ca7d650d3d8406988b2
paulineonier <pauline.onier@outlook.fr>
2024-05-17 19:53:21 +0000

    task 1 correction

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""class square."""
+# 0-square.py
+"""Define a class Square."""
 
 class Square:
-    def __init__(self, size):
-
-"""private instance size."""
-
-        self.__size = size
+    """Represent a square."""
+    pass
