3c1bdfaebc0c72fbd17a662699b7716d2381ffb2
paulineonier <pauline.onier@outlook.fr>
2024-05-23 13:54:12 +0000

    task 0 correction

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 0-lookup
+Contains method lookup that returns list of object's attribute and methods
 
+"""
 def lookup(obj):
-    
-    """ Function that returns the list of available attributes
-        and methods of an object
-
-    Args:
-        obj: instance of the class
-
-    Returns:
-        List of attributes
-    """
+    """returns list of object's attribute and methods"""
     return dir(obj)
