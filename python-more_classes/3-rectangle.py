b59b2648bf04d1c0911ccec3f3209e53829d2e52
paulineonier <pauline.onier@outlook.fr>
2024-05-23 11:14:39 +0000

    task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
-"""
-Module 3-square
-Defines class Square with private attribute size and public attribute area
+# 3-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
-"""
+
 class Square:
-    """
-    class Square definition
-
-    Args:
-        size (int): size of a side in square
-
-    Functions:
-        __init__(self, size)
-        area(self)
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """
-        Initializes square
+        """Initialize a new square.
 
-        Attributes:
-            __size (int): size of a side of square, defaults to 0 if none
+        Args:
+            size (int): The size of the new square.
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """
-        Calculates area of square
-
-        Returns:
-            area
-        """
-        return (self.__size)**2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
