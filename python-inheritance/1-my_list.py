e46e0d1847f42396f84a3911e73e79ed722d5b8c
paulineonier <pauline.onier@outlook.fr>
2024-05-23 13:56:58 +0000

    task 1 correction

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""
+Module 1-my_list
+
+Contains class MyList
+inherits from list; has public instance method to print sorted
+
+"""
 class MyList(list):
-    """ Class that inherits the attributes references of class list
+    """inherits from list
 
-    Args:
-        list: class list
-
+    methods:
+    print_sorted(self)
     """
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)i
+        """prints list of ints all sorted in ascending order"""
+        print(sorted(self))
