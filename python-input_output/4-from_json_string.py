ad321a3c0df553a3b6d6e64b484d7e7b8c188583
paulineonier <pauline.onier@outlook.fr>
2024-05-29 13:28:12 +0000

    task 4 correction

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""from_json_string module.
+
+Contains a function that returns an object represented by a JSON string.
+"""
 import json
+
 
 def from_json_string(my_str):
     """
-    Convert a JSON string to a Python object.
-
-    Args:
-    my_str (str): A JSON string representing a Python object.
-
-    Returns:
-    object: The Python object represented by the JSON string.
+    Returns an object (Python data structure)
+    represented by a JSON string:.
     """
     return json.loads(my_str)
