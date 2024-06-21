c6dc237d54e18ac61e382670bc71dd05060c9baf
paulineonier <pauline.onier@outlook.fr>
2024-06-21 13:31:18 +0200

    task 0 correction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
 """
-This module lists all states from the database hbtn_0e_0_usa.
-It takes 3 arguments: mysql username, mysql password, and database name.
-The results are sorted in ascending order by states.id.
+lists all table values
+parameters given to script: username, password, database
 """
 
-import sys
 import MySQLdb
+from sys import argv
 
+if __name__ == "__main__":
 
-def list_states(username, password, database_name):
-    """
-    Connect to the MySQL server running on localhost at port 3306,
-    retrieve all states from the specified database, and print them
-    sorted in ascending order by states.id.
-    
-    Args:
-        username (str): MySQL username
-        password (str): MySQL password
-        database_name (str): Database name
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database_name)
-    
-    cur = db.cursor()
-    
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    rows = cur.fetchall()
-    
-    for row in rows:
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
         print(row)
-    
-    cur.close()
+    cursor.close()
     db.close()
