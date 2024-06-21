ace8ec999a9c3e6d4d84f1a6e51f92ed180dc5d4
paulineonier <pauline.onier@outlook.fr>
2024-06-21 23:17:50 +0200

    task 7 correction

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+return all state objects from database via python
+parameters given to script: username, password, database
+"""
 
 from sys import argv
 from model_state import Base, State
@@ -7,17 +11,27 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    # Ensure correct number of command-line arguments
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
 
-    # make engine for database
+    # Retrieve command-line arguments
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
+
+    # Create engine for connecting to the database
+    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db}', pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in session.query(State).order_by(State.id):
-        print("{:d}: {:s}".format(instance.id, instance.name))
-
-    session.close()
+    try:
+        # Query all State objects and print their id and name
+        for instance in session.query(State).order_by(State.id):
+            print(f"{instance.id}: {instance.name}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the session
+        session.close()
